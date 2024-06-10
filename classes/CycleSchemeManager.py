fb91a6f11c626ff97343d18485aa597754acca95
Stephan <Stephanf2003@gmail.com>
2024-06-10 20:50:11 +0200

    added promptGenerator, implemented methods

--- classes/CycleSchemeManager.py
+++ classes/CycleSchemeManager.py
@@ -1,31 +1,47 @@
+from typing import Dict
 from ai.AiClient import AiClient
 
 from classes.CycleInfoExtractor import CycleInfoExtractor
+from classes.PromptGenerator import PromptGenerator
 
 from cycle_types.CycleInfo import CycleInfo
 from cycle_types.CycleScheme import CycleScheme
 
 
 class CycleSchemeManager:
-    def __init__(
-        self, aiClient: AiClient, cycleInfoExtractor: CycleInfoExtractor
-    ) -> None:
+    def __init__(self, aiClient: AiClient, pdf_path: str) -> None:
         self.aiClient = aiClient
-        self.cylceInfoExtractor = ciExtractor
+        self.cycleInfoExtractor = CycleInfoExtractor(pdf_path, aiClient)
 
-    def does_scheme_exist(self, number: int) -> bool:
-        pass
-        return False
-        # Checks if scheme in storage
+    def get_scheme(self, cycleNum: int) -> CycleScheme:
+        """Returns a scheme from the storage. If none exists it generates a new one using the the given ai and given documentation
 
-    def add_scheme(self, cycleInfo: CycleInfo) -> None:
+        Args:
+            :param ``cycleNum``: The number of the requested cycle
+        """
+        if scheme := self._get_scheme_from_storage(cycleNum):
+            return scheme
+        scheme = self._generate_scheme_from_ai(cycleNum)
+        # self._store_scheme(scheme)
+        return scheme
+
+    def _get_scheme_from_storage(self, cycleNum: int) -> CycleScheme | None:
+        if False:  # Get scheme from storage
+            return CycleScheme(cycleNum)
+        return None
+
+    def _generate_scheme_from_ai(self, cycleNum: int) -> CycleScheme:
+        cycleInfo: CycleInfo = self.cycleInfoExtractor.extract_cycle_info(
+            cycleNum)
+
+        prompt = PromptGenerator.generate_with_instructions_and_data(
+            "generate_scheme", cycleInfo
+        )
+        dict_out = self.aiClient.dict_query(prompt=prompt)
+        return dict_out  # type: ignore
+
+        # TODO: store dict in CycleScheme
+
+    def _store_scheme(self, cycleScheme: CycleScheme) -> None:
         pass
         # Adds scheme to storage
-
-    def generate_scheme_from_ai(self, cycleInfo: CycleInfo) -> CycleScheme:
-        pass
-
-    def get_cycle_scheme(self, cycleNum: int) -> CycleScheme:
-        # Get scheme from storage throw error if not found
-
-        pass
