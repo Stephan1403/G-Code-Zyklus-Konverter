4847c3c19834ed7acacc666df8921abc95b1e092
Stephan <Stephanf2003@gmail.com>
2024-06-10 20:50:48 +0200

    added str representation

--- cycle_types/CycleInfo.py
+++ cycle_types/CycleInfo.py
@@ -2,7 +2,27 @@ from typing import List, Dict
 
 
 class CycleInfo:
-    def __init__(self, number: int, steps: List, params: Dict) -> None:
+    def __init__(self, number: int, steps: List, params: Dict, description=""):
         self.number = number
+        self.description = description
         self.steps = steps
         self.params = params
+
+    def __str__(self) -> str:
+        des = ""
+        if len(des) > 0:
+            des = f"Zyklus Beschreibung: {self.description}\n"
+
+        return (
+            des
+            + f"""Zyklus Ablauf:
+        {"\n-".join(self.steps)}
+
+        mit folgenden Zyklus Parametern:
+        {"\n-".join(self.params)}
+
+        """
+        )
+
+    def __repr__(self) -> str:
+        return str(self)
